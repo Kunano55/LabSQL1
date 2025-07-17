d82f380a2c9345307043f06d8d40b2128d3160c0
Kunano55 <kukongkaka@gmail.com>
2025-07-17 15:42:09 +0700

    Add files via upload

---
+++ LabSQL1.sql
@@ -0,0 +1,18 @@
+Select * From Employees
+
+Select EmployeeID, FirstName, LastName
+from Employees
+
+Select *
+From Employees
+Where City = 'london'
+
+Select EmployeeID, FirstName, LastName
+From Employees
+Where City = 'London'
+
+Select City, Country
+From Customers
+
+Select Distinct City, Country
+From Customers
