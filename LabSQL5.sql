660fc602a873c59ba886b3f824d481154d8ae48c
Kunano55 <kukongkaka@gmail.com>
2025-08-21 13:53:16 +0700

    Update LabSQL5.sql

--- LabSQL5.sql
+++ LabSQL5.sql
@@ -1,3 +1,35 @@
 SELECT CategoryName, ProductName, Unitprice
 FROM Products, Categories
 WHERE Products.CategoryID=Categories.CategoryID
+
+SELECT CategoryName, ProductName, Unitprice
+FROM Products as P, Categories as C
+WHERE P.CategoryID=C.CategoryID
+
+SELECT CategoryName, ProductName, Unitprice
+FROM Products JOIN Categories
+ON Products.CategoryID=Categories.CategoryID
+
+SELECT CategoryName, ProductName, Unitprice
+FROM Products as P JOIN Categories as C
+ON P.CategoryID=C.CategoryID
+
+
+SELECT CategoryName, ProductName, Unitprice
+FROM Products as P, Categories as C
+WHERE P.CategoryID=C.CategoryID
+AND CategoryName='seafood'
+
+SELECT CategoryName, ProductName, Unitprice
+FROM Products as P JOIN Categories as C
+ON P.CategoryID=C.CategoryID
+AND CategoryName='seafood'
+
+SELECT CompanyName, OrderID
+FROM Orders JOIN Shippers
+ON Shippers.ShipperID=Orders.ShipVia
+
+SELECT CompanyName, OrderID
+FROM Orders JOIN Shippers
+ON Shippers.ShipperID=Orders.ShipVia
+WHERE OrderID=10275
